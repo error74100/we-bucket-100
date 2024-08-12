a39aedc91808212ebb224d66612bd169055036cf
error7410 <error7410@gmail.com>
2024-08-12 18:27:54 +0900

    readme 수정

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,18 +1,29 @@
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Write() {
   return (
     <>
       <div className="content_nav">
-        <span className="icon_back">뒤로가기{ArrowBackIcon}</span>
+        <Link to="" className="icon_back">
+          뒤로가기
+        </Link>
         <span className="title">제목!!</span>
         <button>취소/완료</button>
       </div>
 
-      <div>Write</div>
-      <div>사진</div>
-      <div>날짜</div>
-      <div>내용</div>
+      <div>
+        <h2 className="h3_type">사진</h2>
+      </div>
+      <div>
+        <h2 className="h3_type">감정아이콘</h2>
+      </div>
+      <div>
+        <h2 className="h3_type">날짜</h2>
+      </div>
+      <div>
+        <h2 className="h3_type">내용</h2>
+      </div>
     </>
   );
 }
